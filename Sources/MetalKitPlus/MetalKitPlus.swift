83bcd400224b1e66c27d56a5d54cc1998267a4ce
Jude <judeeric@icloud.com>
2026-05-16 12:57:03 +1000

    test if main file is neccesary

--- Sources/MetalKitPlus/MetalKitPlus.swift
+++ Sources/MetalKitPlus/MetalKitPlus.swift
@@ -3,8 +3,19 @@
 
 import MetalKit
 
-extension MetalViewPlus {
-    public init (renderLoop: @escaping (MetalRenderLoopController) -> Void) {
-        self.runRenderLoop = renderLoop
-    }
-}
+//extension MetalViewPlus {
+//    public init (renderLoop: @escaping (MetalRenderLoopController) -> Void) {
+//        self.runRenderLoop = renderLoop
+//    }
+//}
+//
+//extension MetalState {
+//    public init(
+//}
+//
+//extension Vertex {
+//    public init(position: SIMD3<Float>, color: SIMD3<Float>) {
+//        self.position = position
+//        self.colorRgb = color
+//    }
+//}
